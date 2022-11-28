3ecf9bffd68d460c25a1b445f198de6a8bd0784d
Humano Laranja <contato.carlos@outlook.com>
2022-11-28 15:55:24 -0300

    build

--- README.md
+++
@@ -1,3 +0,0 @@
-# AM079
-
-Projeto de AM079 para dar visibilidade aos vídeos criados na matéria
--- src/config.js
+++
@@ -1,2 +0,0 @@
-export const apiKey = "AIzaSyBghtp4LZ5NaN-WZ7AXSOWbvI1L1h2Mnsk";
-export const sheetId = "1ceRpA2H5Fvyl0UHn4HV2QumZRHwYZNx5pMXo7CmDYrE";
